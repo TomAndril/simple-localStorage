df46500471bac7f3538d1bb09e176a49d407af3c
Tomas <acatomas@gmail.com>
2019-07-23 19:42:59 -0300

    se agrego boton borrar y mas validadores de campos

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,7 @@ function nameValidator(event) {
     if (inputNode.value != '' && numberInput === true) {
         nombre.classList.remove('is-invalid')
         nombre.classList.add('is-valid')
-        botonAgregar.disabled = false
+        botonValidator()
     } else {
         nombre.classList.add('is-invalid')
         nombre.classList.remove('is-valid')
@@ -31,7 +31,7 @@ function apellidoValidator(event) {
     if (numberInput === true) {
         apellido.classList.remove('is-invalid')
         apellido.classList.add('is-valid')
-        botonAgregar.disabled = false
+        botonValidator()
     } else {
         apellido.classList.add('is-invalid')
         apellido.classList.remove('is-valid')
@@ -44,13 +44,34 @@ function dniValidator(event) {
     if (inputNode.value != '' && inputNode.value > 0 && buscarAlumnoPorDni(inputNode.value) !== true) {
         dni.classList.add('is-valid')
         dni.classList.remove('is-invalid')
-        botonAgregar.disabled = false
+        botonValidator()
     } else {
         dni.classList.remove('is-valid')
         dni.classList.add('is-invalid')
         botonAgregar.disabled = true
     }
 }
+
+function emailValidator(event) {
+    var inputNode = event.target
+    if (inputNode.value !== '' && inputNode.value.indexOf('@') >= 0 && inputNode.value.indexOf('.') >= 0) {
+        email.classList.add('is-valid')
+        email.classList.remove('is-invalid')
+        botonValidator()
+    } else {
+        email.classList.remove('is-valid')
+        email.classList.add('is-invalid')
+        botonAgregar.disabled = true
+    }
+}
+
+function botonValidator() {
+    if (nombre.classList.contains('is-valid') && apellido.classList.contains('is-valid') && dni.classList.contains('is-valid') && email.classList.contains('is-valid')) {
+        botonAgregar.disabled = false
+    }
+}
+
+botonValidator()
 
 // Funcion que encuentra coincidencias con los valores DNI del localStorage y un input field. En caso de encontrar coincidencia, devuelve true
 
@@ -83,21 +104,7 @@ nombre.onblur = nameValidator
 apellido.onblur = apellidoValidator
 dni.onblur = dniValidator
 eliminar.onblur = eliminarValidator
-
-// La funcion de abajo crea un nodo alumno
-
-function crearNodoEstudiante() {
-    var centralParentNode = document.getElementById('mainList')
-    var liNodeCreator = document.createElement('li')
-    var h1NodeCreator = document.createElement('h1')
-    var h3NodeCreator = document.createElement('h3')
-
-    liNodeCreator.className = 'list-group-item'
-
-    centralParentNode.appendChild(liNodeCreator)
-    liNodeCreator.appendChild(h1NodeCreator).innerHTML = 'Nombre: ' + nombre.value + ' ' + apellido.value
-    liNodeCreator.appendChild(h3NodeCreator).innerHTML = 'DNI: ' + dni.value
-}
+email.onblur = emailValidator
 
 
 // la funcion de abajo toma los alumnos existentes en el localStorage y en caso de encontrarlos los agrega al DOM, caso contrario imprime un mensaje
@@ -105,7 +112,7 @@ function crearNodoEstudiante() {
 function mostrarAlumnosExistentes() {
 
     var parsedAlumnos = JSON.parse(listaAlumnos)
-
+    
     if (parsedAlumnos === null) {
 
         console.log('No hay alumnos almacenados en localStorage') // <-- en caso de no encontrar nada dispara este console log
@@ -117,41 +124,69 @@ function mostrarAlumnosExistentes() {
             var liNodeCreator = document.createElement('li')
             var h1NodeCreator = document.createElement('h1')
             var h3NodeCreator = document.createElement('h3')
+            var h3NodeCreator2 = document.createElement('h3')
 
             liNodeCreator.className = 'list-group-item'
+            liNodeCreator.id = parsedAlumnos[i].id
 
             centralParentNode.appendChild(liNodeCreator)
-            liNodeCreator.appendChild(h1NodeCreator).innerHTML = 'Nombre: ' + parsedAlumnos[i].firstName + ' ' + parsedAlumnos[i].lastName
+            liNodeCreator.appendChild(h1NodeCreator).innerHTML = parsedAlumnos[i].firstName + ' ' + parsedAlumnos[i].lastName
             liNodeCreator.appendChild(h3NodeCreator).innerHTML = 'DNI: ' + parsedAlumnos[i].dni
+            liNodeCreator.appendChild(h3NodeCreator2).innerHTML = 'Email: ' + parsedAlumnos[i].email
         }
-    }
+    }   
 }
-mostrarAlumnosExistentes();
+mostrarAlumnosExistentes()
 
-// Se agrega una funcion que sube objetos alumno al localStorage
+
+// La siguiente funcion crea un nodo alumno en el DOM y tambien sube el objeto alumno al localStorage
 
 function nuevoAlumno() {
+    
+    var nodoLista = document.getElementById('mainList')
+    var parsedAlumnos = JSON.parse(localStorage.getItem("Alumno"));
 
-    var parsedAlumnos = JSON.parse(localStorage.getItem("Alumno")); // se crea una variable nueva de alumnos parseados
-
-    if (parsedAlumnos == null) parsedAlumnos = []; // en caso de no encontrar nada dentro del localStorage se crea un array de datos vacio
+    if (parsedAlumnos == null) parsedAlumnos = []; // en caso de no encontrar nada dentro del localStorage se crea un array de datos 
 
     var alumno = {
         firstName: nombre.value,
         dni: dni.value,
         lastName: apellido.value,
+        email: email.value,
+        id: dni.value,
     };
 
     localStorage.setItem("Alumno", JSON.stringify(alumno));
     parsedAlumnos.push(alumno);
     localStorage.setItem("Alumno", JSON.stringify(parsedAlumnos));
 
-    crearNodoEstudiante()
+    var centralParentNode = document.getElementById('mainList')
+    var liNodeCreator = document.createElement('li')
+    var h1NodeCreator = document.createElement('h1')
+    var h3NodeCreator = document.createElement('h3')
+    var h3Nodecreator2 = document.createElement('h3')
+
+
+    liNodeCreator.className = 'list-group-item'
+    liNodeCreator.id = alumno.id
+
+    centralParentNode.appendChild(liNodeCreator)
+    liNodeCreator.appendChild(h1NodeCreator).innerHTML = nombre.value + ' ' + apellido.value
+    liNodeCreator.appendChild(h3NodeCreator).innerHTML = 'DNI: ' + dni.value
+    liNodeCreator.appendChild(h3Nodecreator2).innerHTML = 'Email: ' + email.value
 
     nombre.value = ''
+    nombre.classList.remove('is-valid')
     dni.value = ''
+    dni.classList.remove('is-valid')
     apellido.value = ''
+    apellido.classList.remove('is-valid')
+    dni.value = ''
+    dni.classList.remove('is-valid')
+    email.value = ''
+    email.classList.remove('is-valid')
     botonAgregar.disabled = true
+
 };
 
 botonAgregar.onclick = nuevoAlumno
@@ -159,9 +194,17 @@ botonAgregar.onclick = nuevoAlumno
 // La siguiente funcion elimina un alumno del DOM y el localStorage
 
 function eliminarAlumno() {
+
     var parsedAlumnos = JSON.parse(localStorage.getItem("Alumno"));
+    var nodoAlumno = document.getElementById(eliminar.value)
+
+    nodoAlumno.remove()
+
     for (let i = 0; i < parsedAlumnos.length; i++) {
         const element = parsedAlumnos[i];
+
+        // El siguiente if va a comparar si el dni del array de alumnos coincide con el valor del campo eliminar. En caso de que coincidan, se va a crear un array nuevo SIN el elemento encontrado y se va a subir al localStorage
+
         if (element.dni === eliminar.value) {
             parsedAlumnos = parsedAlumnos.filter(function (alumno) {
                 var arraySinAlumno = alumno.dni !== element.dni
@@ -170,7 +213,11 @@ function eliminarAlumno() {
             localStorage.setItem("Alumno", JSON.stringify(parsedAlumnos));
         }
     }
+
+    eliminar.value = ''
+    eliminar.classList.remove('is-valid')
     botonEliminar.disabled = true
+    
 }
 
 botonEliminar.onclick = eliminarAlumno
